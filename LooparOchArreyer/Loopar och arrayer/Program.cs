5250c74a367a905407dd08392f291eac5d12775a
SetupFailed <thomas.karlsson619@gmail.com>
2024-03-15 11:44:08 +0100

    jksgrhiuserafhiasfiuh

--- LooparOchArreyer/Loopar och arrayer/Program.cs
+++ LooparOchArreyer/Loopar och arrayer/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 
-
 string[] toys = { "Bakugan", "Beyblade", "Omnitrix", "Tamagotchi", "HotWheels" };
 string[] people = { "Sunnie", "Tommi", "Tin", "Zacke", "Nicole" };
 
@@ -9,13 +8,33 @@ foreach (string i in toys)
     Console.WriteLine(i);
 }
 
-/*for (int i = 0; i < toys.Length; i++)
+
+
+int[] num = { 1, 3, 5, 7, 9 };
+
+for (int i = 0; i < toys.Length; i++)
 {
-    Console.WriteLine(people[i] + " har " + toys[i]);
-}*/
+    Console.WriteLine(people[i] + " ger " + toys[i] + " betyget " + num[i]);
+}
 
-int[] num = {1, 3, 5, 7, 9};
 
-for (int i = 0; i < toys.Length; i++) {
-    Console.WriteLine(people[i] + " ger " + toys[i]);
+
+string input = "";
+
+List<string> CitiesList = new();
+
+while (input.ToLower() != "exit") // (lowercase sensitiv4e)
+{
+    Console.WriteLine("Type a city or type 'exit' to exit");
+    input = Console.ReadLine();
+
+    if (input.ToLower() != "exit") // Check if user wants to exit (lowercase sensitive)
+    {
+        CitiesList.Add(input); // Add the city to the list
+    }
+    Console.WriteLine("Cities entered:");
+    foreach (string city in CitiesList)
+    {
+        Console.WriteLine(city); // Output the cities entered
+    }
 }
